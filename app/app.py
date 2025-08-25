b2965f9099c3449255333341570ea46caf666681
Andrew Dubov <work@dubov-ai.ru>
2025-08-26 00:16:07 +0300

    CHORE: убрал лишнюю точку входа

--- app/app.py
+++ app/app.py
@@ -1,18 +1,15 @@
 from app.bot import dp, bot
 from app.controllers import subscription_manager
+from app.db import init_db
 from app.handlers import router
 
 
 async def main():
+    init_db()
+
     dp.include_router(router)
 
     # Очищаем истёкшие подписки при запуске
     subscription_manager.cleanup_expired_subscriptions()
 
     await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import asyncio
 
 from app.app import main
-from app.db import init_db
 
 if __name__ == "__main__":
-    init_db()
-
     asyncio.run(main())
