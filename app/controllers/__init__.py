191957ef47b875385e8dc7443cbfd99dd3dfa374
Andrew Dubov <work@dubov-ai.ru>
2025-08-25 23:34:19 +0300

    CHORE

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,6 +1,5 @@
-from statistics import BotStatistics
-
 from chat_settings import ChatSettingsManager
+from statistics_controller import BotStatistics
 from subscription_manager import SubscriptionManager
 from user_triggers import UserTriggerManager
 
